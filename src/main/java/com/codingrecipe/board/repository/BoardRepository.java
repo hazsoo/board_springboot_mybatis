5e901405ec8d352b1f944bb75885911d0c93bbf8
hazsoo <cmlh21@gmail.com>
2024-06-03 15:01:28 +0900

    list method

--- src/main/java/com/codingrecipe/board/repository/BoardRepository.java
+++ src/main/java/com/codingrecipe/board/repository/BoardRepository.java
@@ -5,11 +5,17 @@ import lombok.RequiredArgsConstructor;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class BoardRepository {
     private final SqlSessionTemplate sql;
     public void save(BoardDTO boardDTO) {
         sql.insert("Board.save", boardDTO); // Board: mapper namespace, save: 쿼리문을 담고 있는 태그(id)
+    }
+
+    public List<BoardDTO> findAll() {
+        return sql.selectList("Board.findAll");
     }
 }
